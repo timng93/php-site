1a89c6ab1f4d3414f65bbba5b94ff6a3c4f0e02d
Timmy <timgabrielnguyen@gmail.com>
2019-04-12 16:56:02 -0700

    Working with Strings

--- site.php
+++ site.php
@@ -9,11 +9,18 @@
 <body>
 
 <?php 
-      $phrase = "To be or not to be";
-       $age = 30;
-       $gpa = 30.0;
-       $isMale = false;
-?>
-    
+      $phrase = "Timmy World";
+      echo strtolower($phrase);
+      echo "<br>";
+      echo strlen($phrase);
+      echo "<br>";
+      $phrase[0] = "J";
+      echo $phrase;
+      echo "<br>";
+      echo str_replace("Jimmy", "Momoa", $phrase);
+      echo "<br>";
+      echo substr($phrase, 6, 3);
+
+?>    
 </body>
 </html>
