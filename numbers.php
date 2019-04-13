f484359c1fddf24f3c153a71a220383786030a48
Timmy <timgabrielnguyen@gmail.com>
2019-04-12 17:11:24 -0700

    Different Operations

---
+++ numbers.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
+      echo 5 + 9;
+      echo "<br>";
+      echo 5.7 * 9;
+      echo "<br>";
+      echo 10 % 3;
+      echo "<br>";
+       echo (4 +5) * 10;
+?>
+    
+</body>
+</html>
