34d26ebb72ae01bd2f11b7ee036ee20e459277c1
Timmy <timgabrielnguyen@gmail.com>
2019-04-14 00:28:54 -0700

    Set Up for Loops

---
+++ for-loops.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   $luckyNumbers= array(4,8,16,32);
+   for($i = 0; $i < count($luckyNumbers); $i++) {
+       echo "$luckyNumbers[$i] <br>";
+   }
+   
+  
+   ?>
+    
+</body>
+</html>
